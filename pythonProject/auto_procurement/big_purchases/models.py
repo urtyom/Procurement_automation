1f24368727ba55542b9ef88ebcf2ce63fc89ddba
urtyom <dianalokk01@yandex.ru>
2024-05-14 09:58:35 +0300

    add model Profile

--- pythonProject/auto_procurement/big_purchases/models.py
+++ pythonProject/auto_procurement/big_purchases/models.py
@@ -1,42 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Shop(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
     url = models.URLField()
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+
+
 class Category(models.Model):
-    name = models.CharField(max_length=255)
-    shops = models.ManyToManyField(Shop)
+    name = models.CharField(max_length=100)
+    shops = models.ManyToManyField(Shop, related_name='categories')
 
 
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name='products')
 
 
 class ProductInfo(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     price_rrc = models.DecimalField(max_digits=10, decimal_places=2)
 
 
 class Parameter(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
 
 
 class ProductParameter(models.Model):
     product_info = models.ForeignKey(ProductInfo, on_delete=models.CASCADE)
     parameter = models.ForeignKey(Parameter, on_delete=models.CASCADE)
-    value = models.CharField(max_length=255)
+    value = models.CharField(max_length=100)
 
 
 class Order(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     dt = models.DateTimeField()
     status = models.CharField(max_length=50)
 
@@ -50,5 +57,5 @@ class OrderItem(models.Model):
 
 class Contact(models.Model):
     type = models.CharField(max_length=50)
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    value = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    value = models.CharField(max_length=100)
